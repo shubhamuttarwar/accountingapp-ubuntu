074ac0fbb3c48d083586f2dc5efcbd0974df8c7f
shubham uttarwar <shubhamuttarwar14@gmail.com>
2017-10-17 23:48:14 +0530

    added the destroy functionality and added the index image

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,13 @@ class ProjectsController < ApplicationController
     @project = @customer.projects.create(project_params)
     redirect_to customer_path(@customer)
   end
- 
+    
+    def destroy
+    	@customer = Customer.find(params[:customer_id])
+    	@project = @customer.projects.find(params[:id])
+    	@project.destroy
+    	redirect_to customer_path(@customer)
+    end
   private
     def project_params
       params.require(:project).permit(:jname, :jdescription, :price)
