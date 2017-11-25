9059b6fe35ee75b34e609bae62c02dcb718f35fb
shubham uttarwar <shubhamuttarwar14@gmail.com>
2017-11-25 16:23:58 +0530

    added edit functionality to addons

--- app/controllers/addons_controller.rb
+++ app/controllers/addons_controller.rb
@@ -1,16 +1,28 @@
 class AddonsController < ApplicationController
-	def index
-	end
+	def show
 
-	def new
-	end
+    @project = Project.find(params[:project_id])
+    @addon = @project.addons.find(params[:id])
+    end
 
 	def create
 		@project = Project.find(params[:project_id])
 		@addon = @project.addons.create(addons_params)
 		redirect_to customer_project_path(@project.customer_id, @project)
 	end
+
+  def edit
+    @project = Project.find(params[:project_id])
+    @addon = @project.addons.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @addon = @project.addons.update(addons_params)
+    redirect_to customer_project_path(@project.customer_id,@project)
+  end
     
+
 
      def destroy
      	@project = Project.find(params[:project_id])
