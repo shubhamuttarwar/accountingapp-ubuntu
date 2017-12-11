b3f63b423a2f7fc8552c8c5f7700edc7c59e0b4e
Shubham Uttarwar <shubhamuttarwar14@gmail.com>
2017-12-11 12:00:09 +0530

    resolved wkhtmltopdf issue and migrated to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209051316) do
+ActiveRecord::Schema.define(version: 20171209050857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
