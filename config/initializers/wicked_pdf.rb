4600cf37a448c3d18691fdd12f38a5f84c6f2087
Shubham Uttarwar <shubhamuttarwar14@gmail.com>
2017-12-16 14:06:51 +0530

    tried footer for pdf but did not add

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,7 +14,7 @@ WickedPdf.config = {
   #exe_path: '/Documents/wkhtmltox/bin/wkhtmltopdf',
   #   or
   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
+  
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
